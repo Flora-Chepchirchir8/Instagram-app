e06496696be4067293ac305615081bcb127cc77d
Flora-Chepchirchir8 <flora.chepchirchir@moringaschool.com>
2022-06-07 11:13:48 +0300

    configure urls.py

--- picsinsta/urls.py
+++ picsinsta/urls.py
@@ -1,7 +1,7 @@
 """picsinsta URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, re_path
+from django.contrib.auth import views 
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+  re_path('admin/', admin.site.urls),
+  re_path('',include('insta.urls')),
+  re_path(r'accounts/', include('registration.backends.simple.urls')),
+    #url(r'logout/', views.LogoutView.as_view(), {"next_page": 'accounts/login'})
+  re_path(r'logout/', views.logout_then_login),
 ]
