625f0a3017908e5f296e262bb8c31fdd21827407
Flora-Chepchirchir8 <flora.chepchirchir@moringaschool.com>
2022-06-07 18:50:26 +0300

    fix bugs

--- insta/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Image,Comment,Profile
-# Register your models here.
-admin.site.register(Profile)
-admin.site.register(Image)
-admin.site.register(Comment)
